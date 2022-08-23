079d5dc66311e279ec5858ea3fe041893e0e93b1
Jeremy-Vaud <100855862+Jeremy-Vaud@users.noreply.github.com>
2022-08-24 00:03:38 +0200

    fix z-index + code formating

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -4,17 +4,17 @@
 
 export function init_tabs() {
     $(".tabs > li").on("click", change_tabs)
-    $(".tabs-active").each(function() {
-        $("#"+$(this).attr("for")).show()
+    $(".tabs-active").each(function () {
+        $("#" + $(this).attr("for")).show()
     })
 }
 
 function change_tabs() {
-    if(!$(this).hasClass("tabs-active")){
+    if (!$(this).hasClass("tabs-active")) {
         let active = $(this).parent().children(".tabs-active")
         active.removeClass("tabs-active")
         $(this).addClass("tabs-active")
-        $("#"+active.attr("for")).hide()
-        $("#"+$(this).attr("for")).show()
+        $("#" + active.attr("for")).hide()
+        $("#" + $(this).attr("for")).show()
     }
 }
